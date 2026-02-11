8231a715a892468a8b0bc4f7a7c9086a126ae5e1
Ozodbek Kamolov <ozodbekkamolov@gmail.com>
2026-02-12 00:49:26 +0100

    Create install script for Zsh and Oh-My-Zsh setup

---
+++ install.sh
@@ -0,0 +1,36 @@
+# update system
+sudo apt update
+sudo apt upgrade -y
+
+# install zsh
+sudo apt install zsh
+
+# make it default shell
+chsh -s $(which zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install plugins: zsh-autosuggestions, zsh-syntax-highlighting, zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+# add plugins to ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
+
+# install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# replace .p10k.zsh with kamoloff_p10k.zsh
+cp p10k.zsh ~/.p10k.zsh
+
+# add source ~/.p10k.zsh to ~/.zshrc
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
+
+# update the theme
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+
+# restart zsh
+source ~/.zshrc
