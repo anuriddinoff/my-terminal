ec89e54a35abe8339ded77a005f44c0d65574048
Abdulalim Nuriddinov <nuriddinoff2007@gmail.com>
2026-02-24 23:51:46 +0500

    fix: make install script compatible with macOS

--- install.sh
+++ install.sh
@@ -1,36 +1,34 @@
-# update system
-sudo apt update
-sudo apt upgrade -y
+#!/bin/zsh
 
-# install zsh
-sudo apt install zsh
+# install zsh (macOS da allaqachon bor, lekin brew orqali yangilash)
+brew install zsh
 
 # make it default shell
 chsh -s $(which zsh)
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# install plugins: zsh-autosuggestions, zsh-syntax-highlighting, zsh-autocomplete
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
-# add plugins to ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
+# add plugins to ~/.zshrc (macOS sed requires '')
+sed -i '' 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
 
 # install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-# replace .p10k.zsh with anuriddinoff_p10k.zsh
+# copy p10k config
 cp p10k.zsh ~/.p10k.zsh
 
-# add source ~/.p10k.zsh to ~/.zshrc
+# add to ~/.zshrc
 echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
-
-# update the theme
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 # restart zsh
-source ~/.zshrc
+exec zsh
